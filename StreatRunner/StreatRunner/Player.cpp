d9e81981bb23ad280b2ce1748c0c7f020936698f
ketuimoko <ketuimoko@gmail.com>
2015-08-15 15:42:42 +0900

    add PlayerAttack

--- StreatRunner/StreatRunner/Player.cpp
+++ StreatRunner/StreatRunner/Player.cpp
@@ -1,6 +1,7 @@
 #include "Player.h"
 #include"PlayerMove.h"
 #include"PlayerJump.h"
+#include "PlayerAttack.h"
 
 Player::Player(Side side)
 {
@@ -9,6 +10,7 @@ Player::Player(Side side)
 
 	state_ = MOVE;
 	playerState_ = std::make_shared<PlayerMove>();
+	attack_ = nullptr;
 }
 
 
@@ -22,6 +24,9 @@ void Player::update()
 		else if (side_ == LEFT && Input::KeyW.pressed) {
 			playerState_ = std::make_shared<PlayerJump>(this->rect().x, this->rect().y, -50);
 		}
+		else if ((side_ == RIGHT && Input::KeyDown.pressed) || (side_ == LEFT && Input::KeyS.pressed)){
+			playerState_ = std::make_shared<PlayerAttack>();
+		}
 		else {
 			playerState_ = std::make_shared<PlayerMove>();
 		}
@@ -32,6 +37,10 @@ void Player::update()
 	
 }
 
+std::shared_ptr<Rect>& Player::getAttack()
+{
+	return attack_;
+}
 
 void Player::changeState()
 {
