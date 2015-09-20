73ccc821a124f49ddca9fedf37f9c10311e4ff80
Jumpaku <itzimeru@gmail.com>
2015-09-20 09:39:14 +0900

    Runner が画面から出ないようにした 攻撃が同時の時お互いが死ぬ様にした

--- StreetRunner/StreetRunner/CollisionPlayerAttack.cpp
+++ StreetRunner/StreetRunner/CollisionPlayerAttack.cpp
@@ -10,10 +10,15 @@ CollisionPlayerAttack::~CollisionPlayerAttack()
 
 void CollisionPlayerAttack::detect(Player &player1, Player &player2)
 {
-	if (player1.getAttack() != nullptr && player1.getAttack()->intersects(player2.rect())) {
+	bool colliding2 = 
+		player1.getAttack() != nullptr && player1.getAttack()->intersects(player2.rect());
+	bool colliding1 =
+		player2.getAttack() != nullptr && player2.getAttack()->intersects(player1.rect());
+
+	if (colliding2) {
 		player2.initialize();
 	}
-	if (player2.getAttack() != nullptr && player2.getAttack()->intersects(player1.rect())) {
+	if (colliding1) {
 		player1.initialize();
 	}
 }
--- StreetRunner/StreetRunner/PlayerMove.cpp
+++ StreetRunner/StreetRunner/PlayerMove.cpp
@@ -13,21 +13,27 @@ bool PlayerMove::hasFinished()
 void PlayerMove::update(Player &player)
 {
 	if (player.side() == Player::LEFT) {
-		if (Controller::input_->rightPressed(player.side()))
-		{
-			player.rect().moveBy(2, 0);
+		if (Controller::input_->rightPressed(player.side()))	{
+			auto x = player.rect().x;
+			player.rect().x = Clamp(x + 2, 0, 1280-25);
+			//player.rect().moveBy(2, 0);
 		}
 		else if (Controller::input_->leftPressed(player.side())) {
-			player.rect().moveBy(-2, 0);
-
+			auto x = player.rect().x;
+			player.rect().x = Clamp(x - 2, 0, 1280 - 25);
+			//player.rect().moveBy(-2, 0);
 		}
 	}
 	else {
 		if (Controller::input_->rightPressed(player.side())) {
-			player.rect().moveBy(2, 0);
+			auto x = player.rect().x;
+			player.rect().x = Clamp(x + 2, 0, 1280 - 25);
+			//player.rect().moveBy(2, 0);
 		}
 		else if (Controller::input_->leftPressed(player.side())) {
-			player.rect().moveBy(-2, 0);
+			auto x = player.rect().x;
+			player.rect().x = Clamp(x - 2, 0, 1280 - 25);
+			//player.rect().moveBy(-2, 0);
 		}
 	}
 }
